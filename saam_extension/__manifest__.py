fc6f77a26534092d7f7860101113935c0afaa2a3
Sathish-0901 <sathishkmr12345@gmail.com>
2023-05-29 20:53:43 +0530

    code has been added

---
+++ saam_extension/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Saam Extension',
+    'version': '15.0.1',
+    'category': '',
+    'summary': 'Saam Extension',
+    
+    'author': 'SSL',
+    'website': '',
+    'maintainer': '',
+
+    'depends': ['base','product'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/partner.xml',
+        'views/product.xml',
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'price': 110.00,
+    'currency': 'EUR',
+    'images': ['static/description/icon.png'],
+    'license': 'AGPL-3',
+}
