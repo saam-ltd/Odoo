e47df82497e6642db2b9bdad69ee71350a2fa0be
samm-ltd <saamodoo@gmail.com>
2024-03-21 14:43:50 +0530

    Aarthi - Code commented at backdate

--- stock_picking_backdate_all/wizard/change_to_backdate.py
+++ stock_picking_backdate_all/wizard/change_to_backdate.py
@@ -10,9 +10,9 @@ _logger = logging.getLogger(__name__)
 class StockPickingInherit(models.Model):
     _inherit = 'stock.picking'
 
-    def _set_scheduled_date(self):
-        for picking in self:
-            picking.move_lines.write({'date': picking.scheduled_date})
+    # def _set_scheduled_date(self):
+    #     for picking in self:
+    #         picking.move_lines.write({'date': picking.scheduled_date})
 
 
 class PickingBackDate(models.TransientModel):
