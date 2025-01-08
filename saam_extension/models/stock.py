d04695bc86e681d8055f4f67e4242892d3a39e16
samm-ltd <saamodoo@gmail.com>
2025-01-08 18:16:48 +0530

    Aarthi - code selection added at picking for outgoing

--- saam_extension/models/stock.py
+++ saam_extension/models/stock.py
@@ -25,6 +25,7 @@ class Picking(models.Model):
     customer_remarks =  fields.Text(string='Customer Remarks',related='partner_id.cus_remarks', tracking=2,copy=False)
 
     is_date_updated = fields.Boolean(string='Is Date Updated')
+    code = fields.Selection(related='picking_id.picking_type_code', string='Picking Type - Code', store=True)
 
 
 
